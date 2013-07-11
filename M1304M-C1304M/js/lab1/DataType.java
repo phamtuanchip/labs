9936c20170a8c94717c2cab5dbfd1c8c14a94f98
pham tuan <phamtuanchip@gmail.com>
2013-07-12 00:42:24 +0700

    update data type

---
+++ M1304M-C1304M/js/lab1/DataType.java
@@ -0,0 +1,26 @@
+
+public class DataType {
+	int i;
+	long l;
+	short s;
+	float f;
+	double d;
+	boolean b;
+	char c;
+	
+	String st;
+	String[] sa;
+	Integer I;
+	Long L;
+	Short S;
+	Double D;
+	Boolean B;
+	
+	Class cl ;
+	DataType dt ;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ M1304M-C1304M/js/lab1/Params.java
@@ -0,0 +1,10 @@
+public class Params {
+	
+	public static void main(String[] args) {
+		if(args != null) {
+			for (String s : args) {
+				System.out.println(s);
+			}
+		}
+	}
+}
