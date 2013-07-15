50d01fc873d5b5131c2005d501296419117ee10e
Tuan Pham <tuanp@exoplatform.com>
2013-07-15 15:20:27 +0700

    add lab

--- T1306L/epc/lab1/Hello.c
+++ T1306L/epc/lab1/Hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int a;
 int b;
@@ -10,6 +11,26 @@ void showInfor(){
 
 int add2Number(int a, int b) {
 	return a + b;
+}
+
+void inputData(){
+	printf("\nWhat is your name?: \n");
+	char name[100];
+	int age ;
+	scanf("%[^\n]s", name) ;
+	printf("Welcome %s to programing with C\n", name);
+	fflush(stdout);
+	printf("How old are you?: \n");
+	scanf("%d", &age) ;
+
+	if(age < 16 ) {
+		printf("You are supper! you can learn C very early\n");
+	} else if(age < 35) {
+		printf("Do you work for any company?\n");
+	} else if(age > 35){
+		printf("Are you programer?\n");
+	}
+
 }
 
 int main(){
@@ -24,4 +45,5 @@ int main(){
 
 	printf("Add 2 numbers : %d and %d we have %d\n", a, b, add2Number(a,b));
 
+	inputData();
 }
