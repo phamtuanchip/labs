ec361c4f2a651b79527c8c637765127c511f1bd5
Tuan Pham <tuanp@exoplatform.com>
2013-07-17 11:27:43 +0700

    add xample for phone

---
+++ M1304M-C1304M/js/lab3/SamSungSmartPhone.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2003-2013 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package lab3;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ *          exo@exoplatform.com
+ * Jul 17, 2013  
+ */
+public class SamSungSmartPhone extends CellPhone implements Smart {
+
+  @Override
+  public void receiveCall() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void makeCall() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void sendSMS() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void receiveSMS() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public String getOSInformation() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void installedOS(String OSName) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
