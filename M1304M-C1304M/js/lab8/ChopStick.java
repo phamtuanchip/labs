97c856a6f0db8b11242edc458a760baecf0e7a68
Tuan Pham <tuanp@exoplatform.com>
2013-08-01 18:27:11 +0700

    add example

---
+++ M1304M-C1304M/js/lab8/ChopStick.java
@@ -0,0 +1,29 @@
+package lab7;
+
+class ChopStick
+{
+	boolean available;
+	ChopStick()
+	{
+		available = true;
+	}
+	public synchronized void takeup()
+	{
+		while(!available)
+		{
+			try
+			{
+				System.out.println("Philosopher is waiting for the other chopstick");
+				wait();
+			}
+			catch(InterruptedException e)
+			{ }
+		}
+		available = false;
+	}
+	public synchronized void putdown()
+	{
+		available = true;
+    	 	notify();
+	}
+}
---
+++ M1304M-C1304M/js/lab8/Dining.java
@@ -0,0 +1,23 @@
+package lab7;
+
+class Dining
+{
+	static ChopStick[] chopsticks = new ChopStick[5];
+	static Philosopher[] philos = new Philosopher[5];
+	
+	public static void main(String args[])
+	{
+		for (int count = 0;count <= 4;count++)
+		{
+			chopsticks[count] = new ChopStick();
+		}
+     
+		for (int count = 0;count <= 4;count++)
+		{
+			philos[count] = new Philosopher(count,chopsticks[count],chopsticks[(count+1)%5]);
+		}
+    	
+		for (int count = 0;count <= 4;count++)
+			philos[count].start( );
+	}
+}
