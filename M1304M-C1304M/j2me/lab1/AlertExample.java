81f5d7f7004ebf601f1ae528ee21ba0497313486
Tuan Pham <tuanp@exoplatform.com>
2013-09-19 17:47:12 +0700

    update example and book lab1

---
+++ M1304M-C1304M/j2me/lab1/AlertExample.java
@@ -0,0 +1,63 @@
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.lcdui.*;
+
+public class AlertExample extends MIDlet implements CommandListener{
+	private Display display;
+	public Form form;
+	private Command ok,cancel,done;
+	private Image img, imge, img2;
+			
+	public AlertExample() {
+		form = new Form("Roseindia Alert");
+		cancel = new Command("Cancel", Command.CANCEL, 2);
+		ok = new Command("OK", Command.OK, 2);
+		try{
+			img = Image.createImage("/logo.png");
+			imge = Image.createImage("/front_left1_bad.png");
+			img2 = Image.createImage("/Congratulations-1.png");
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}		
+	}
+
+ 	public void startApp() {
+		display = Display.getDisplay(this);
+		try{form.append(img);}catch(Exception e){}
+		form.addCommand(cancel);
+		form.addCommand(ok);
+		form.setCommandListener(this);
+		display.setCurrent(form);
+	}
+
+	public void pauseApp() {}
+
+	public void destroyApp(boolean unconditional) {
+		notifyDestroyed();
+	}
+
+	public void showMsg(){
+		Alert success = new Alert("OK", "Alert Message Here!", img2, AlertType.INFO);
+		success.setTimeout(5000);
+		success.setImage(img2);
+		display.setCurrent(success, form);		
+	}
+
+	public void tryAgain() {
+		Alert error = new Alert("Cancel", "Alert Message Here!", imge, AlertType.INFO);
+		error.setTimeout(5000);
+		error.setImage(imge);
+		display.setCurrent(error, form);
+	}
+	
+	public void commandAction(Command c, Displayable d) {
+		String label = c.getLabel();
+		if(label.equals("Cancel")) {
+			tryAgain();
+		} else if(label.equals("OK")) {
+			showMsg();
+		} 
+	}
+}
+
+
+
---
+++ M1304M-C1304M/j2me/lab1/SoundAlertExample.java
@@ -0,0 +1,63 @@
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+
+public class SoundAlertExample extends MIDlet implements ItemStateListener, CommandListener{
+	private Display display;    
+	private Form form;      
+	private Command exit;     
+	private ChoiceGroup choice; 
+
+	public void startApp(){
+		display = Display.getDisplay(this);
+		choice = new ChoiceGroup("List of Sound", Choice.EXCLUSIVE);
+		choice.append("Message Tone", null);    
+		choice.append("Confirmation Tone", null);    
+		choice.append("Warning Tone", null);            
+		choice.append("Alarm Tone", null);        
+		choice.append("Error Tone", null);    
+		exit = new Command("Exit", Command.EXIT, 1);
+		form = new Form("");
+		form.append(choice);
+		form.addCommand(exit);
+		form.setCommandListener(this);   
+		form.setItemStateListener(this);
+		display.setCurrent(form);
+	}
+
+	public void pauseApp(){ }
+
+	public void destroyApp(boolean unconditional){
+		notifyDestroyed();
+	}
+
+	public void commandAction(Command c, Displayable s){
+		String label = c.getLabel();
+		if(label.equals("Exit")){
+			destroyApp(false);
+		} 
+	}
+
+	public void itemStateChanged(Item item){
+		switch (choice.getSelectedIndex()){
+			case 0: 
+				AlertType.INFO.playSound(display);
+			break;
+
+			case 1:
+				AlertType.CONFIRMATION.playSound(display);
+			break;
+
+			case 2:
+				AlertType.WARNING.playSound(display);
+			break;
+			
+			case 3:
+				AlertType.ALARM.playSound(display);
+			break;
+
+			case 4:
+				AlertType.ERROR.playSound(display);
+			break;
+		}
+	}
+}
