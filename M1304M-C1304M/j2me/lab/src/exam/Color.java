750677831a9b37cb959e4e20c1ed7f88655e75b0
Tuan Pham <tuanp@exoplatform.com>
2013-10-15 17:46:12 +0700

    update resource

--- M1304M-C1304M/j2me/lab/src/exam/Color.java
+++
@@ -1,7 +0,0 @@
-package exam;
-
-public class Color 
-{
-public static final int WHITE = 0x00ffffff;
-public static final int BLACK = 0x00000000;
-}
--- M1304M-C1304M/js/lab6/FieldEmptyException.java
+++
@@ -1,9 +0,0 @@
-package layout;
-
-public class FieldEmptyException extends Exception {
-	@Override
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return "Truong du lieu khong duoc de trong!";
-	}
-}
